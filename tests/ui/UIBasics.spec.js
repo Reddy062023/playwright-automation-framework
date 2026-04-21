5cada566b1a20a4f6d186563f615d89c25e6eabb
Geetu <japendareddy@gmail.com>
2026-04-21 14:29:38 -0400

    fix: convert UIBasics to ES module import syntax

--- tests/ui/UIBasics.spec.js
+++ tests/ui/UIBasics.spec.js
@@ -1,13 +1,13 @@
-const { test, expect } = require("@playwright/test");
+﻿import { test, expect } from "@playwright/test";
 
-test('Browser context Playwright test', async ({ browser }) => {
+test("Browser context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
 });
 
-test('First Playwright test', async ({ page }) => {
+test("First Playwright test", async ({ page }) => {
   await page.goto("https://google.com");
   console.log(await page.title());
   await expect(page).toHaveTitle("Google");
